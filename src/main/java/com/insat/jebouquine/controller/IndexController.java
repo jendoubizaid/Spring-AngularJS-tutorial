f3443d9c724c5c840ec1e306737e0dd068c372c3
zaid jendoubi <jendoubizaid@gmail.com>
2016-11-10 16:33:23 +0100

    remove comments

--- src/main/java/com/insat/jebouquine/controller/IndexController.java
+++ src/main/java/com/insat/jebouquine/controller/IndexController.java
@@ -8,23 +8,19 @@ public class IndexController {
 
 	@RequestMapping("/")
 	public String indexPage(){
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>im in index controller");
 		return "redirect:/main";
 	}
 	
 	@RequestMapping("/main")
 	public String mainPage(){
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>im in main page index controller");
 		return "main";
 	}
 	@RequestMapping("/partials/books")
 	public String partialBooksPage(){
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>im in partial books index controller");
 		return "partials/books";
 	}
 	@RequestMapping("/partials/checkout")
 	public String partialCheckoutPage(){
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>im in checkout index controller");
 		return "partials/checkout";
 	}
 	@RequestMapping("/mycategory/")
@@ -33,32 +29,26 @@ public class IndexController {
 	}
 	@RequestMapping("/checkout/")
 	public String checkCart(){
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>im in checkout controller");
 		return "checkout";
 	}
 	@RequestMapping("/partials/addUser")
 	public String addUser(){
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>im in add user controller");
 		return "partials/addUser";
 	}
 	@RequestMapping("/partials/bookDetails")
 	public String detailsBook(){
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>im in book details controller");
 		return "partials/bookDetails";
 	}
 	@RequestMapping("/partials/booksByKeyword")
 	public String keywordSearchBook(){
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>im in book details controller");
 		return "partials/booksByKeyword";
 	}
 	@RequestMapping("/partials/booksByCategory")
 	public String booksByCat(){
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>im in book details controller");
 		return "partials/booksByCategory";
 	}
 	@RequestMapping("/admin/category")
 	public String addCategory(){
-		System.out.println(">>>>>>>>>>>>>>>>>>>>>im in book details controller");
 		return "admin/category";
 	}
 	
